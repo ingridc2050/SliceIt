a7673f3548a4d88005b79de54815bd7444283d05
Ingrid Carlson <icarlson@carroll.edu>
2025-03-06 11:35:39 -0700

    Added Bomb and Player skeleton classes

---
+++ sliceit/Bomb.java
@@ -0,0 +1,18 @@
+package sliceit;
+
+public class Bomb {
+	private boolean bombHit = false;
+	
+	
+	
+	private void bombIsHit() {
+		bombHit = true;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
